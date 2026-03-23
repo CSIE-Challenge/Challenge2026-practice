8da684643820c8682c155c371ad1ebefcd19d217
Chaoray <chaoray_ice@outlook.com>
2026-03-23 21:34:06 +0800

    feat: add random mines and safe first click

--- minesweeper/game/board.py
+++ minesweeper/game/board.py
@@ -4,7 +4,9 @@ from dataclasses import dataclass
 
 from minesweeper.utils.helpers import count_adjacent_mines
 
+import random
 
+'''
 DEFAULT_MINES = {
     (1, 1),
     (5, 1),
@@ -13,6 +15,7 @@ DEFAULT_MINES = {
     (2, 6),
     (7, 7),
 }
+'''
 
 
 @dataclass(slots=True)
@@ -25,12 +28,16 @@ class Cell:
     adjacent_mines: int = 0
 
 
-def create_board(width: int, height: int) -> list[list[Cell]]:
+def create_board(width: int, height: int, mine_count: int, first_reveal: tuple[int, int]) -> list[list[Cell]]:
     # TODO: Randomize mine placement after the first click and guarantee the first
     # move is safe. The starter project uses a fixed board so students can focus on
     # Git collaboration before implementing real game setup logic.
+
+    all_possible_mine_positions = [(x, y) for x in range(width) for y in range(height) if (x, y) != first_reveal]
+    mine_positions = random.sample(all_possible_mine_positions, k=mine_count)
+
     board = [
-        [Cell(x=x, y=y, has_mine=(x, y) in DEFAULT_MINES) for x in range(width)]
+        [Cell(x=x, y=y, has_mine=(x, y) in mine_positions) for x in range(width)]
         for y in range(height)
     ]
 
